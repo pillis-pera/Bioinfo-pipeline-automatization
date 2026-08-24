85194aa7e96336781386f6dac75374ed9420b255
pillis-pera <pillispera@SWAN-IV.localdomain>
2026-08-24 18:49:35 +0200

    miRNA pipeline with working sorting and indexing step

--- Local/Pipeline.sh
+++ Local/Pipeline.sh
@@ -29,38 +29,43 @@ REFERENCE_DIR="${REPO_DIR}/Reference"
 # 	echo ""
 # done
 
-cd $UMI_EXTRACTED_DIR
+# cd $UMI_EXTRACTED_DIR
 # bowtie-build "${REFERENCE_DIR}/hsa_mature_DNA.fa" "${REFERENCE_DIR}/hsa_mature_index"
-> ${ALIGNED_DIR}/mapping_logs/mapping_stats.txt
-for file in *.fastq*; do
-	base="${file%.umi_extracted.fastq*}"
-	#creates a variable with the name of the file without the extention to name all subsequent files created from this .fastq
+# > ${ALIGNED_DIR}/mapping_logs/mapping_stats.txt
+# for file in *.fastq*; do
+# 	base="${file%.umi_extracted.fastq*}"
+# 	#creates a variable with the name of the file without the extention to name all subsequent files created from this .fastq
 	
-	echo "Sample: $base" >> "${ALIGNED_DIR}/mapping_logs/mapping_stats.txt"
-	#writes the name of the file (without extension) to a new line in the .txt file
+# 	echo "Sample: $base" >> "${ALIGNED_DIR}/mapping_logs/mapping_stats.txt"
+# 	#writes the name of the file (without extension) to a new line in the .txt file
 	
-	bowtie -q -S "${REFERENCE_DIR}/hsa_mature_index" "$file" > "${ALIGNED_DIR}/${base}.sam" 2>> "${ALIGNED_DIR}/mapping_logs/temporal_bowtie_output.txt"
-	# -q for .fastq files (change it to the corresponding option accordingly, check bowtie), this line writes the termianls output into a temporal .txt file for later grepping and creates a .sam file from the mapping
+# 	bowtie -q -S "${REFERENCE_DIR}/hsa_mature_index" "$file" > "${ALIGNED_DIR}/${base}.sam" 2>> "${ALIGNED_DIR}/mapping_logs/temporal_bowtie_output.txt"
+# 	# -q for .fastq files (change it to the corresponding option accordingly, check bowtie), this line writes the termianls output into a temporal .txt file for later grepping and creates a .sam file from the mapping
 	
-	result=$(grep '^#' "${ALIGNED_DIR}/mapping_logs/temporal_bowtie_output.txt")
-	#bowties terminal oputput gives a small summary of the results with each line of the summary starting with a '#', this code exploits this to grep only those lines and peg them to the variable 'results'
+# 	result=$(grep '^#' "${ALIGNED_DIR}/mapping_logs/temporal_bowtie_output.txt")
+# 	#bowties terminal oputput gives a small summary of the results with each line of the summary starting with a '#', this code exploits this to grep only those lines and peg them to the variable 'results'
 	
-	#echo "$result"                 #UNCOMMENT IF YOU WANT TO SEE THE MAPPING RESULTS ON TERMINAL IN REAL TIME
-	echo "$result" >> "${ALIGNED_DIR}/mapping_logs/mapping_stats.txt"
-	echo "------------------------------" >> "${ALIGNED_DIR}/mapping_logs/mapping_stats.txt"
-	#adds the summary and a separator to the .txt file for easy sample identification
+# 	#echo "$result"                 #UNCOMMENT IF YOU WANT TO SEE THE MAPPING RESULTS ON TERMINAL IN REAL TIME
+# 	echo "$result" >> "${ALIGNED_DIR}/mapping_logs/mapping_stats.txt"
+# 	echo "------------------------------" >> "${ALIGNED_DIR}/mapping_logs/mapping_stats.txt"
+# 	#adds the summary and a separator to the .txt file for easy sample identification
 	
-	> ${ALIGNED_DIR}/mapping_logs/temporal_bowtie_output.txt
+# 	> ${ALIGNED_DIR}/mapping_logs/temporal_bowtie_output.txt
 
-	echo "file: $file done"
-	#feedbacks into the terminal to notify the user that the file has been processed
-done
+# 	echo "file: $file done"
+# 	#feedbacks into the terminal to notify the user that the file has been processed
+# done
 
-
-	# #clears the temporal .txt where the bowtie output is being stored so the new file in the for loop can write on it from cero
-	# samtools view -bS "$outdiremapping/${base1}.sam" | samtools sort -o "$outdiremapping/${base1}.sorted.bam"
-	# #takes the .bam that was just created with bowtie and creates its sorted.bam version
+# cd $ALIGNED_DIR
+# for file in *.sam; do
+# 	base="${file%.sam}"
+# 	#clears the temporal .txt where the bowtie output is being stored so the new file in the for loop can write on it from cero
+# 	samtools view -bS "$file" | samtools sort -o "${SORTED_DIR}/${base}.sorted.bam"
+# 	#takes the .bam that was just created with bowtie and creates its sorted.bam version
 	
-	# samtools index "$outdiremapping/${base1}.sorted.bam"
-	# #creates the sorted.bam.bai version of the .bam file so it can be manipulated with something like umitools
+# 	samtools index "${SORTED_DIR}/${base}.sorted.bam"
+# 	#creates the sorted.bam.bai version of the .bam file so it can be manipulated with something like umitools
+#     # echo $base
+#     # echo $file
+# done
 	
