f8afc66e593f79aa719bb844ef0a3c45751d1073
pillis-pera <pillispera@SWAN-IV.localdomain>
2026-09-10 22:11:53 +0200

    Apply fold-change threshold to DESeq2 results
    Define configurable adjusted p-value and fold-change thresholds. Refactor the analysis script to improve readability and use the LFC threshold when testing the alternative hypothesis.

--- Analysis/R/differential_expression.R
+++ Analysis/R/differential_expression.R
@@ -1,23 +1,89 @@
-setwd("/path/to/directory/Bioinfo-pipeline-automatization/Analysis") #<-- Set this to your wd.
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(DESeq2)
-counts <- read.csv('counts_matrix_filtered.csv', row.names=1)
-coldata <- read.csv('Metadata/metadata.csv', row.names = 1)
+setwd("/path/to/Bioinfo-pipeline-automatization") #<-- Set this to your wd.
 
-coldata <- coldata[colnames(counts),]
+####################
+# Import libraries #
+####################
+suppressPackageStartupMessages({
+    library(dplyr)
+    library(DESeq2)}
+)
 
-dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~Sex + Condition)
+#################
+# Configuration #
+#################
+padj_threshold <- 0.05
+fc_threshold <- 1.5
+lfc_threshold <- log2(fc_threshold)
+
+
+#############
+# Load data #
+#############
+counts_matrix <- read.csv('Analysis/counts_matrix_filtered.csv', row.names=1, check.names=FALSE)
+sample_metadata <- read.csv('Analysis/Metadata/metadata.csv', row.names = 1)
+sample_metadata <- sample_metadata[colnames(counts_matrix),]
+
+####################
+# Prepare metadata #
+####################
+sample_metadata$sex <- factor(sample_metadata$sex)
+sample_metadata$condition <- factor(sample_metadata$condition)
+sample_metadata$condition <- relevel(
+  factor(sample_metadata$condition),
+  ref = "C"
+)
+
+################
+# DESeq2 model #
+################
+dds <- DESeqDataSetFromMatrix(
+    countData = counts_matrix, 
+    colData = sample_metadata, 
+    design = ~sex + condition
+)
 dds <- DESeq(dds)
-vst <- varianceStabilizingTransformation(dds)   
-NCounts <- counts(dds, normalized = T)          #Normalized counts
-log2_NCounts <- log2(NCounts+1)                 #Log2 of normalized counts
 
-res <- results(dds)
-resOrdered <- res[order(res$padj), ]
-sig <- res[which(res$padj < 0.05 & abs(res$log2FoldChange) >= log2(1.5)), ]
-sig <- sig[order(sig$padj),]
+###################################
+# Normalized / transformed counts #
+###################################
+vst_counts <- varianceStabilizingTransformation(dds)   
 
-miRNAs <- row.names(sig[grep("miR|let",row.names(sig)),])
-miRNAs_res <- sig[grep("miR|let",row.names(sig)),]
+normalized_counts <- counts(
+    dds, 
+    normalized = TRUE
+)          #Normalized counts
+
+log2_normalized_counts <- log2(normalized_counts+1)                 #Log2 of normalized counts
+
+###########
+# Results #
+###########
+results_deseq <- results(
+    dds,
+    alpha = padj_threshold,
+    lfcThreshold = lfc_threshold,
+    altHypothesis = "greaterAbs"
+)
+
+results_deseq <- results_deseq[order(results_deseq$padj), ]
+
+#########################
+# Significant smallRNAs #
+#########################
+significant_results <- results_deseq[
+    which(
+        results_deseq$padj < padj_threshold 
+        & 
+        abs(results_deseq$log2FoldChange) >= lfc_threshold
+    ), 
+]
+
+######################
+# Significant miRNAs #
+######################
+significant_mirnas <- row.names(
+    significant_results[
+        grep("miR|let",row.names(significant_results)
+        ),
+    ]
+)
